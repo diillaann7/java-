b488559df80f8872196204c31f20b8d902cc03d5
dilan <dilan.othman2005@gmail.com>
2025-09-03 16:45:06 +0200

    neu

--- 5_projekt/demo/src/main/java/com/example/demo/entity/produkte.java
+++ 5_projekt/demo/src/main/java/com/example/demo/entity/produkte.java
@@ -1,11 +1,13 @@
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "produkte")
